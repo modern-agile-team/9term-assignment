e9adf9c09ca363f8145c9311ff55d5ebd07b25e6
yihimin <yihimin01@gmail.com>
2024-12-03 17:47:16 +0900

    서울에서 김서방 찾기

--- day1/findKim.js
+++ day1/findKim.js
@@ -1,8 +1,8 @@
 const seoul = ['Jane', 'Kim'];
 
 function solution(seoul){
-    const index = seoul.indexOf('Kim');
-    return `김서방은 ${index}에 있다`
+    const i = seoul.indexOf('Kim');
+    return `김서방은 ${i}에 있다`
 }
 
 console.log(solution(seoul));
