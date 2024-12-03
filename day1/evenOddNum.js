bf13afd0b92f36cf4cb713baf020e98d89de5938
yihimin <yihimin01@gmail.com>
2024-12-03 17:46:04 +0900

    짝수와 홀수

--- day1/evenOddNum.js
+++ day1/evenOddNum.js
@@ -3,25 +3,5 @@ function solution(num) {
 }
 
 
-
-
-
-//실행환경
-const readline = require("readline");
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-
-rl.question("정수를 입력하세요: ", (input) => {
-    const num = parseInt(input, 10); 
-    if (isNaN(num)) {
-        console.log("유효한 정수를 입력해주세요."); 
-    } else {
-        console.log(`결과: ${solution(num)}`); 
-    }
-
-    rl.close(); 
-});
+console.log(solution(2));
+console.log(solution(3));
