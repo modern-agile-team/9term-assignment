f35034ed3088a0eb32c3f020af3f88c61c51b450
seungmin Kang <kangsengmin@naver.com>
2024-12-04 14:14:53 +0900

    2일차 과제

--- ksm/even_odd.js
+++ ksm/even_odd.js
@@ -1,7 +1,16 @@
-function solution(num) {
+/*function solution(num) {
     let even="Even";
     let odd="Odd";
     if (num%2==0) {
+        return even;
+    }
+    return odd;
+}*/
+
+function solution(num) {
+    let even = "Even";
+    let odd = "Odd";
+    if (num % 2 == 0) {
         return even;
     }
     return odd;
