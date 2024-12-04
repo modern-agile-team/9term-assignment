ee1eb1d9e67ed2154967981a7d29583a6524a259
KBH <alxnfkfkfk@naver.com>
2024-12-04 18:35:07 +0900

    2일차 피드백 수정

--- KBH/2Day/가운데 글자 가져오기.js
+++ KBH/2Day/가운데 글자 가져오기.js
@@ -1,6 +1,6 @@
 function solution(s) {
     let answer = '';
-    const slice = s.length / 2;
+    const middleIndex = s.length / 2;
     //2로 나누어서 짝수, 홀수 구분하기위한 상수 선언
     answer = (s.length % 2) === 0 ? s.substr(slice-1,2) : s.substr(slice ,1);
     return answer;
