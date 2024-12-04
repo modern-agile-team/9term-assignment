c1cc5596956168762cc36464f62a73a866de3123
yihimin <yihimin01@gmail.com>
2024-12-04 19:01:46 +0900

    style: apply convention

--- day1/문자열내p와y의개수.js
+++ day1/문자열내p와y의개수.js
@@ -1,10 +1,14 @@
-function solution(s){
+function solution(s) {
     let countP = 0;
     let countY = 0;
 
-    for (const char of s.toLowerCase()){
-        if(char === 'p') countP++;
-        if(char === 'y') countY++
+    for (const char of s.toLowerCase()) {
+        if (char === 'p') {
+            countP++;
+        }
+        if (char === 'y') {
+            countY++;
+        }
     };
 
     return countP === countY;
