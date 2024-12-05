498f1d69f7451b6242ee8bdd456669fbd67ced78
yihimin <yihimin01@gmail.com>
2024-12-05 17:18:02 +0900

    fix: convention and equal operator to strict

--- himin/day2/두 정수 사이의 합.js
+++ himin/day2/두 정수 사이의 합.js
@@ -1,11 +1,12 @@
 function solution(a, b) {
-    let countAB = 0;
-    
     const min = Math.min(a, b);
     const max = Math.max(a, b);
+
+    let countAB = 0;
     
     for(let i = min ; i <= max; i++){
         countAB += i
     }
+    
     return countAB;
 }
