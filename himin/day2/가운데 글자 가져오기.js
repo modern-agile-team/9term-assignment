9cdc470b4e71324b15d01ae73e1cbaec787449fb
Yihimin <127826452+yihimin@users.noreply.github.com>
2024-12-05 18:32:02 +0900

    Update himin/day2/가운데 글자 가져오기.js
    Co-authored-by: Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>

--- himin/day2/가운데 글자 가져오기.js
+++ himin/day2/가운데 글자 가져오기.js
@@ -2,10 +2,10 @@ function solution(s) {
     const middleIndexS = Math.floor(s.length / 2);
     let middleChar = "";
       
-   if(s.length % 2 === 1) {
+   if (s.length % 2 === 1) {
       middleChar = s[middleIndexS];
    } else {
-         middleChar = s[middleIndexS - 1] + s[middleIndexS];
+      middleChar = s[middleIndexS - 1] + s[middleIndexS];
    }
       
       return middleChar;
