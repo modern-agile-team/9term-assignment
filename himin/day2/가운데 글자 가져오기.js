5e8079e8510147fc5031e770a5b0a45bf1c2a4d5
yihimin <yihimin01@gmail.com>
2024-12-05 11:36:55 +0900

    resolve: middle letter

---
+++ himin/day2/가운데 글자 가져오기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    let middleIndexS = Math.floor(s.length / 2)
+    let middleChar = "";
+      
+      if(s.length % 2 == 1) {
+         middleChar = s[middleIndexS];
+      } else {
+         middleChar = s[middleIndexS - 1] + s[middleIndexS]
+      }
+      
+      return middleChar;
+  }
