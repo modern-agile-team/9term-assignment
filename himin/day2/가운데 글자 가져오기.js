61be937674c6a53f02fee22edfb7c0c34f078d98
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-12-05 18:38:21 +0900

    Update himin/day2/가운데 글자 가져오기.js

--- himin/day2/가운데 글자 가져오기.js
+++ himin/day2/가운데 글자 가져오기.js
@@ -2,11 +2,11 @@ function solution(s) {
     const middleIndexS = Math.floor(s.length / 2);
     let middleChar = "";
       
-   if (s.length % 2 === 1) {
-      middleChar = s[middleIndexS];
-   } else {
-      middleChar = s[middleIndexS - 1] + s[middleIndexS];
-   }
+    if (s.length % 2 === 1) {
+        middleChar = s[middleIndexS];
+    } else {
+        middleChar = s[middleIndexS - 1] + s[middleIndexS];
+    }
       
-      return middleChar;
-  }
+    return middleChar;
+}
