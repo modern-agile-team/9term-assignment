42f327366d7caa2097f571810a48ef14106e494c
yihimin <yihimin01@gmail.com>
2024-12-06 10:36:04 +0900

    fix: s -->  kthNumber

--- himin/day3/K번째수.js
+++ himin/day3/K번째수.js
@@ -1,12 +1,12 @@
 function solution(array, commands) {
-    let s = [];
+    const kthNumber = [];
       
     for (let command of commands){
         const [i, j, k] = command;
-        let sliceArray = array.slice(i - 1, j);
-        let sortArray = sliceArray.sort( (a, b) => a - b);
+        const sliceArray = array.slice(i - 1, j);
+        const sortArray = sliceArray.sort( (a, b) => a - b);
         s.push(sortArray[k - 1]);
     }
       
-    return s;
+    return kthNumber;
   }
