141a8251f1d4db35226df8db2ae1d4ad05b1134d
yihimin <yihimin01@gmail.com>
2024-12-06 16:47:40 +0900

    fix: 오타

--- himin/day4/로또의 최고 순위와 최저 순위.js
+++ himin/day4/로또의 최고 순위와 최저 순위.js
@@ -1,10 +1,10 @@
-function solution(lottos, win_nums){
-    const rank = [6, 6, 5, 4, 3, 2, 1];
-    const zeroCount = lottos.filter(num => num === 0).length;
-    const matchCount = lottos.filter( num => win_nums.include(num) ).length;
-
+function solution(lottos, win_nums) {
+    const rank = [6, 6, 5, 4, 3, 2, 1]; 
+    const zeroCount = lottos.filter(num => num === 0).length; 
+    const matchCount = lottos.filter(num => win_nums.includes(num)).length; 
+    
     const maxRank = rank[matchCount + zeroCount];
-    const minRank = rank[zeroCount];
-
+    const minRank = rank[matchCount];
+    
     return [maxRank, minRank];
 }
