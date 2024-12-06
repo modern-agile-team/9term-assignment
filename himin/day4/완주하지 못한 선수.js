2714c0892cb4442165d9f66286cc99c36c71655b
yihimin <yihimin01@gmail.com>
2024-12-06 13:39:34 +0900

    fix: convention

--- himin/day4/완주하지 못한 선수.js
+++ himin/day4/완주하지 못한 선수.js
@@ -1,16 +1,16 @@
 function solution(participant, completion) {
-    personCount = {};
-
-    for (person of participant) {
+    const personCount = {};
+    
+    for (const person of participant) {
         personCount[person] = (personCount[person] || 0) + 1;
     }
-
-    for (person of completion) {
-        personCount[person] -= 1; 
+    
+    for (const person of completion) {
+        personCount[person] -= 1;
     }
-
-    for (person of participant) {
-        if (personCount[person] > 0) {
+    
+    for (const person of participant) {
+        if (personCount[person] > 0){
             return person;
         }
     }
