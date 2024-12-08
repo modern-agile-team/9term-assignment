63434aa0c22a4355b9706db8c086b9e7f7e95fde
KBH <alxnfkfkfk@naver.com>
2024-12-09 01:13:15 +0900

    김병현 4일차 과제 수정

--- KBH/3Day/K번째수.js
+++ KBH/3Day/K번째수.js
@@ -1,14 +1,12 @@
 function solution(array, commands) {
     const answer = [];
 
-    let  kNumber= "";
-
     for (let i = 0; i < commands.length; i++){
-    const indexi = commands[i][0];
-    const indexj = commands[i][1];
-    const indexk = commands[i][2];
+    const indexI = commands[i][0];
+    const indexJ = commands[i][1];
+    const indexK = commands[i][2];
 
-    kNumber = array.slice(indexi-1, indexj).sort((a, b) => a-b).slice(indexk-1, indexk);
+    const kNumber = array.slice(indexi-1, indexj).sort((a, b) => a-b).slice(indexk-1, indexk);
         answer.push(kNumber[0]);
     }
 
