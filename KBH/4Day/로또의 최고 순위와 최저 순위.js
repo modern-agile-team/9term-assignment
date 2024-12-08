286b944cb7ab91a64bdf8f60405d3219fe2a5b3c
KBH <alxnfkfkfk@naver.com>
2024-12-08 20:59:05 +0900

    김병현 4일차 과제 수정

--- KBH/4Day/로또의 최고 순위와 최저 순위.js
+++ KBH/4Day/로또의 최고 순위와 최저 순위.js
@@ -4,20 +4,14 @@ function solution(lottos, win_nums) {
     let zeroCount = 0; // 인덱스 값 안에 0일 경우 담는 변수
     // lottosSet과 win_numSet 둘 다 같은 인덱스에 위치하기 위함
     for (let i = 0; i < lottos.length; i++){
-        if (lottos.includes(win_nums[i])){
-            console.log(lottos.includes(win_nums[i]))
-            count += 1;
-        } 
-    }
-    for (let i = 0; i < lottos.length; i++){
         if (lottos[i] === 0){
             zeroCount += 1;
-        }
-    }
-    
+        } else if (win_nums.includes(lottos[i]))
+            count += 1;
+        } 
     const maxPlace = 7 - (count + zeroCount);
     const minPlace = 7 - count;
     answer.push(maxPlace > 6 ? 6 : maxPlace);
     answer.push(minPlace > 6 ? 6 : minPlace);
     return answer;
-}
+    }
