d2cc6b70d173a77acedced89a9f6fc1987135f94
lee224hs <lee224hs@gmail.com>
2024-12-08 18:10:08 +0900

    3일차 과제

--- LHS/1Day/짝수와홀수.js
+++ LHS/1Day/짝수와홀수.js
@@ -1,8 +1,7 @@
-function solution(s){
-    var answer = true;
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log('Hello Javascript')
-
-    return answer;
-}
+function solution(num) {
+    if(num % 2 === 0) {
+        return "Even"
+    } else {
+       return "Odd"
+    }
+ }
