ab6573e9f877eecce1133efb1da856861c60d395
KBH <alxnfkfkfk@naver.com>
2024-12-09 17:15:29 +0900

    김병현 5일차 과제 수정(2)

--- KBH/5Day/음양 더하기.js
+++ KBH/5Day/음양 더하기.js
@@ -12,6 +12,7 @@ function solution(absolutes, signs) {
     return answer;
 }
 // for문을 이용
+// const answer = [];
 // for (let i = 0; i < absolutes.length; i++) {
 //         if (signs[i] === true) {
 //             answer.push((+ absolutes[i]));
