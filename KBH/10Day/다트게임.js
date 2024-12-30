fba17ec6f4ee12225d0ca70ea8f32b9275b54573
KBH <alxnfkfkfk@naver.com>
2024-12-30 10:00:49 +0900

    김병현 11일차, 12일차 과제

---
+++ KBH/10Day/다트게임.js
@@ -0,0 +1,52 @@
+function solution(dartResult) {
+
+    const rounds = dartResult.match(/\d+[SDT][*#]?/g);
+
+    let scores = [];
+
+    for (let i = 0; i < rounds.length; i++) {
+        const round = rounds[i];
+        const score = Number(round.match(/\d+/g));
+        const bonus = round.match(/[SDT]/g)[0];
+
+        //옵션 값 뽑아오기기
+        const hasOption = round.includes('*') || round.includes('#');
+        const option = hasOption ? round[round.length - 1] : undefined;
+        
+        //회차별로 점수 값 저장하는 변수수
+        let roundscore = score;
+
+        switch (bonus) {
+            case 'S' :
+                roundscore = score;
+                break;
+            case 'D' :
+                roundscore = score ** 2;
+                break;
+            case 'T' :
+                roundscore = score ** 3;
+                break;
+            default :
+                continue;
+        }
+
+        if (option === "*") {
+
+            roundscore *= 2;
+            
+            if(i > 0) {
+                
+                scores[i - 1] *= 2;
+            
+            }
+        } else if ( option === "#") {
+                
+            roundscore *= (-1);
+        
+        }
+        
+        scores.push(roundscore);
+
+    }
+    return scores[0] + scores[1] + scores[2];;
+}
