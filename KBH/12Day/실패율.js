8b55c1331f5b5f192440ac63c2fa39d4b1c364bd
KBH <alxnfkfkfk@naver.com>
2024-12-31 12:19:11 +0900

    김병현 12일차 과제

---
+++ KBH/12Day/실패율.js
@@ -0,0 +1,35 @@
+function solution(N, stages) {
+    const answer = [];
+    const copyedStages = [...stages];
+    //실패한 스테이지 개수 확인
+    const failedStage = copyedStages.reduce((acc, cur) => { acc[cur] = (acc[cur] || 0 ) + 1; 
+        return acc;
+    }, {});
+    
+    //다음 스테이지에 올라간 유저수
+    let stageLength = stages.length;
+    //실패율 변수 선언
+    let failedRate = [];
+
+
+    copyedStages.sort();
+
+    for (let i = 1; i <= N; i++) {
+        //해당 객체로 접근해서 사용자의 수를 찾음.
+        const failedCount = failedStage[i] || 0;
+        //실패율 계산
+        failedRate.push({stage : i, rate : failedCount / stageLength});
+        //실패율 계산 후 다음 스테이지에 올라온 유저를 빼기 위해 변수 선언
+        stageLength -= failedCount;
+
+    }
+    //sort 함수를 이용해서 rate의 값을 기준으로 내림차순 진행
+    failedRate.sort((a, b) => b.rate - a.rate);
+
+    //for문을 이용해서 각 인덱스마다 stage요소로 접근해 answer 배열에 저장
+    for (let j = 0; j < failedRate.length; j++) {
+        answer.push(failedRate[j].stage);
+    }
+
+    return answer;
+}
