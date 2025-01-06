9afe2b1701f0ea8119597b28f6c113e461a2a46f
seungmin Kang <kangsengmin@naver.com>
2025-01-06 10:53:37 +0900

    13일차: 불필요한 괄호 제거, 함수명 개선, 공백 정리

--- ksm/ksm_day13/키패드 누르기.js
+++ ksm/ksm_day13/키패드 누르기.js
@@ -19,12 +19,12 @@ function solution(numbers, hand) {
         }
         // 나머지 2 (가운데 열: 2, 5, 8, 0)
         else {
-            const x = (num) => Math.floor((num - 1) / 3); // 행 계산 함수
-            const y = (num) => (Math.floor((num - 1)) % 3); // 열 계산 함수
+            const getColumn = (num) => Math.floor((num - 1) / 3); // 행 계산 함수
+            const getRow = (num) => Math.floor((num - 1) % 3); // 열 계산 함수
 
             // 현재 숫자에서 왼손, 오른손 위치 간의 맨해튼 거리 계산
-            const left = Math.abs(x(cur) - x(handPosition.left)) + Math.abs(y(cur) - y(handPosition.left));
-            const right = Math.abs(x(cur) - x(handPosition.right)) + Math.abs(y(cur) - y(handPosition.right));
+            const left = Math.abs( getColumn(cur) - getColumn(handPosition.left) ) + Math.abs( getRow(cur) - getRow(handPosition.left) );
+            const right = Math.abs( getColumn(cur) - getColumn(handPosition.right) ) + Math.abs( getRow(cur) - getRow(handPosition.right) );
             
             if (left > right) { // 왼손이 더 멀면 
                 acc.push("R"); // 오른손으로 처리
