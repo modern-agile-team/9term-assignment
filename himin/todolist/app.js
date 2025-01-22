686c63086a08c388de9a5b83299d1f227b2156d5
yihimin <yihimin01@gmail.com>
2025-01-22 16:13:21 +0900

    app.js 루트경로 연결

--- himin/todolist/app.js
+++ himin/todolist/app.js
@@ -1,11 +1,20 @@
 require("dotenv").config(); // .env 파일의 내용 로드
 
 const express = require("express");
-const app = express();
+const path = require("path");
 
-// 환경 변수 사용
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, "views"))); // "views" 폴더를 정적 폴더로 설정
+
+// 루트 경로에서 index.html 제공
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+// 서버 시작
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
